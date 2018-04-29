391947dd35c4f9bb7e422b8253f6aacbefee064b
Daniel Hnyk <hnykda@gmail.com>
2018-04-29 09:32:11 +0200

    upgrade deps

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import find_packages, setup
 
 setup(
     name='kfsims',
-    version='0.1.0',  # always SemVer2
+    version='1.0.0', 
     packages=find_packages(),
-    description="informative description",
+    description="Library implementing Distributed VBAKF",
     python_requires=">=3.6",
     licence="MIT",
-    url='https://github.com/GlobalWebIndex/appname',
+    url='https://github.com/hnykda/kfsmis',
     install_requires=[
-        "networkx",
-        "matplotlib",
-        "seaborn",
-        "pandas",
-        "numpy",
-        "scipy",
-        "filterpy",
+        "filterpy==1.2.1",
+        "matplotlib==2.2.2",
+        "networkx==2.1",
+        "numpy==1.14.2",
+        "pandas==0.22.0",
+        "scipy==1.0.0",
+        "seaborn==0.8.1",
     ],
 )
